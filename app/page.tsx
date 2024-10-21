ccaeeb724ea6d7db58e8632805831483a2c10308
Raghav Sharma <47208119+raghav1205@users.noreply.github.com>
2024-10-21 17:45:57 +0530

    client-only hook fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import HomeScreen from "@/components/Home";
 import Navbar from "@/components/Navbar";
 import WalletGenerator from "@/components/WalletGenerator";
-
+import { useIsClient } from "@uidotdev/usehooks";
 export default function Home() {
   const handleScroll = (multiple: number = 1) => {
     console.log("scrolling");
@@ -10,13 +10,14 @@ export default function Home() {
       top: window.innerHeight * multiple,
       behavior: "smooth",
     });
-  }
+  };
+  const isClient = useIsClient();
   return (
     <div className="  w-full ">
       <div className="md:max-w-[80rem] mx-auto w-[90%] ">
         <Navbar />
-        <HomeScreen handleScroll={handleScroll}/>
-        <WalletGenerator handleScroll={handleScroll} />
+        <HomeScreen handleScroll={handleScroll} />
+        {isClient && <WalletGenerator handleScroll={handleScroll} />}
       </div>
     </div>
   );
