b9a70aaba2c9bbdacf8d612a7062ecff8afda4de
Raghav Sharma <47208119+raghav1205@users.noreply.github.com>
2024-10-21 18:33:01 +0530

    timeout fix

--- components/MnemonicDisplay.tsx
+++ components/MnemonicDisplay.tsx
@@ -1,20 +1,27 @@
-import {useState} from 'react'
-
+import { useState } from "react";
 
 interface MnemonicDisplayProps {
   mnemonic: string[];
   handleProceedClick: (id: string) => void;
-  
 }
-const MnemonicDisplay = ({ mnemonic, handleProceedClick }: MnemonicDisplayProps) => {
-  const [cardClicked, setCardClicked] = useState<boolean>(false)
+const MnemonicDisplay = ({
+  mnemonic,
+  handleProceedClick,
+}: MnemonicDisplayProps) => {
+  const [cardClicked, setCardClicked] = useState<boolean>(false);
   const handleCopyMnemonic = () => {
     navigator.clipboard.writeText(mnemonic.join(" "));
+    setCardClicked(true);
+    setTimeout(() => {
+      setCardClicked(false);
+    }, 2000);
   };
 
   return (
     <>
-      <h3  className="text-4xl mb-3 text-center font-semibold">Secret Recovery Phrase</h3>
+      <h3 className="text-4xl mb-3 text-center font-semibold">
+        Secret Recovery Phrase
+      </h3>
       <p className="text-lg text-white text-opacity-60 mb-6">
         Save these words in a safe place
       </p>
@@ -22,7 +29,7 @@ const MnemonicDisplay = ({ mnemonic, handleProceedClick }: MnemonicDisplayProps)
         className="md:w-[60%] bg-[#363844] rounded-md p-2 mb-[1rem]"
         onClick={handleCopyMnemonic}
       >
-        <div className="grid grid-cols-3 items-cetner gap-5 bg-[#363844] rounded-t rouned-md  w-full pl-6 p-3 py-4 ">
+        <div className="grid grid-cols-3 items-cetner gap-5 bg-[#363844] rounded-t rouned-md  w-full pl-6 p-3 py-4 " >
           {mnemonic.map((word, index) => {
             return (
               <div
@@ -37,19 +44,18 @@ const MnemonicDisplay = ({ mnemonic, handleProceedClick }: MnemonicDisplayProps)
             );
           })}
         </div>
-        <p className="w-[98%] mx-auto border-[#494b55] border-t flex justify-center  pt-2 text-sm text-opacity-60 text-gray-100" onClick = {() => {
-          setCardClicked(true)
-          setTimeout(() => {
-            setCardClicked(false)
-        },5000)
-        }}>
-         { cardClicked ? 'Copied!'
-          : 'Click anywhere on this card to copy'
-         }
+        <p
+          className="w-[98%] mx-auto border-[#494b55] border-t flex justify-center  pt-2 text-sm text-opacity-60 text-gray-100"
+          
+        >
+          {cardClicked ? "Copied!" : "Click anywhere on this card to copy"}
         </p>
       </div>
 
-      <button className=" btn btn-green mt-[3rem]" onClick={() => handleProceedClick('addWallet')}>
+      <button
+        className=" btn btn-green mt-[3rem]"
+        onClick={() => handleProceedClick("addWallet")}
+      >
         Proceed
       </button>
     </>
