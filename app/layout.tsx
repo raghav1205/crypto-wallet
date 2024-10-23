e4d8da1cb7305e8aea4d2935e0b8a0b972c75532
Raghav Sharma <47208119+raghav1205@users.noreply.github.com>
2024-10-23 14:14:10 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import {Inter} from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
@@ -20,6 +20,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
